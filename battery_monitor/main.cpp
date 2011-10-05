81bcdb4a53183fe739d358e86215dd09c7983374
Warwick Hunter <w.hunter@computer.org>
2011-10-05 16:40:00 +1000

    A few small improvements

--- battery_monitor/main.cpp
+++ battery_monitor/main.cpp
@@ -17,16 +17,19 @@
 #include "PowerSave.h"
 #include <WString.h>
 
-static const int LED_PIN     = 13; // Pin 13 has an LED connected on most Arduino boards:
-static const int DHT11_PIN   = 4;  // Digital PIN 4 to which the DHT11 temp sensor is attached
-static const int VOLTAGE_PIN = 0;  // Analog input pin to which the voltage divider is attached
+const int           PAUSE        = 20;
+const unsigned long SERIAL_SPEED = 115200;
+
+const int LED_PIN      = 13; // Pin 13 has an LED connected on most Arduino boards:
+const int DHT11_PIN    = 4;  // Digital PIN 4 to which the DHT11 temp sensor is attached
+const int VOLTAGE_PIN  = 0;  // Analog input pin to which the voltage divider is attached
 
 Voltage     voltage(VOLTAGE_PIN);
 Temperature temperature(DHT11_PIN);
 PowerSave   powerSave;
 
 void setup() {
-    Serial.begin(115200);
+    Serial.begin(SERIAL_SPEED);
     pinMode(LED_PIN, OUTPUT);
     voltage.setup();
     temperature.setup();
@@ -50,7 +53,7 @@ void loop() {
     Serial.println();
     Serial.flush();
     // Give the serial output a chance to complete before sleeping
-    delay(20);
+    delay(PAUSE);
 
     digitalWrite(LED_PIN, LOW);  // set the LED off
 

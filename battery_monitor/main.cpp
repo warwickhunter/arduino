449c08dcfaa728f496c684750df88af98be97d62
Warwick Hunter <w.hunter@computer.org>
2011-10-08 14:02:53 +1000

    Don't place the system into low power mode because with a Xbee module attached isn't not going to save any power.

--- battery_monitor/main.cpp
+++ battery_monitor/main.cpp
@@ -17,24 +17,33 @@
 #include "PowerSave.h"
 #include <WString.h>
 
-const int           PAUSE        = 20;
-const unsigned long SERIAL_SPEED = 9600;
+// Set this to 1 if you want the Arduino to go into low power mode between each sample
+#define WITH_SLEEP 0
+
+const int           SHORT_PAUSE    = 20;
+const unsigned long SERIAL_SPEED   = 9600;
+const long          SLEEP_INTERVAL = 5000;
 
 const int LED_PIN      = 13; // Pin 13 has an LED connected on most Arduino boards:
-const int DHT11_PIN    = 4;  // Digital PIN 4 to which the DHT11 temp sensor is attached
+const int DHT11_PIN    = 7;  // Digital PIN 7 to which the DHT11 temp sensor is attached
 const int VOLTAGE_PIN  = 0;  // Analog input pin to which the voltage divider is attached
 
+long        sequenceNumber = 0;
 Voltage     voltage(VOLTAGE_PIN);
 Temperature temperature(DHT11_PIN);
+
+#if WITH_SLEEP
 PowerSave   powerSave;
-long        sequenceNumber = 0;
+#endif
 
 void setup() {
     Serial.begin(SERIAL_SPEED);
     pinMode(LED_PIN, OUTPUT);
     voltage.setup();
     temperature.setup();
+#if WITH_SLEEP
     powerSave.setup();
+#endif
     Serial.println(F("\n\nBattery Monitor v1.0, build " __DATE__ " " __TIME__));
 }
 
@@ -56,10 +65,14 @@ void loop() {
     }
     Serial.println();
     Serial.flush();
-    // Give the serial output a chance to complete before sleeping
-    delay(PAUSE);
 
     digitalWrite(LED_PIN, LOW);  // set the LED off
 
+#if WITH_SLEEP
+    // Give the serial output a chance to complete before sleeping
+    delay(SHORT_PAUSE);
     powerSave.sleep();
+#else
+    delay(SLEEP_INTERVAL);
+#endif
 }

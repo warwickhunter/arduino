c1b8e5b19b77f3bba096ded03408d7b8363848cb
Warwick Hunter <w.hunter@computer.org>
2011-10-08 14:01:07 +1000

    Move the groovy logger into a project

---
+++ logging/ReadArduino.groovy
@@ -0,0 +1,30 @@
+#!/usr/bin/env groovy
+/**
+ * ReadArduino - reads a line of text sent to /dev/ttyUSB1 by the battery monitoring
+ * Arduino over ZigBee and prints the line along with a time stamp.
+ * 
+ * @author Warwick Hunter
+ * @since  2011-10-06
+ */
+
+def cli = new CliBuilder()
+cli.h(longOpt:'help', 'Usage information')
+cli.t(longOpt:'tty', 'The terminal device to open, e.g. /dev/ttyUSB0', required:true, args:1, type:String)
+ 
+def options = cli.parse(args)
+if (!options) {
+    return
+}
+if (options.h) {
+    cli.usage()
+}
+ 
+File tty = new File(options.tty)
+Calendar time = Calendar.getInstance()
+tty.eachLine { line ->
+    if (line.length() > 0) {
+        time.setTimeInMillis(System.currentTimeMillis())
+        printf '%1$tF %1$tT %2$s %n', time, line
+    }
+}
+

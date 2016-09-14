a3e9e6761f3cf2a8eb4c3fbfda2716c347a5b636
Warwick Hunter <w.hunter@computer.org>
2016-09-14 19:44:45 +1000

    Get the temp sensor working. Clone the nametag to make a start on a weather station

--- dht11_temp_sensor/dht_test_pde/dht_test_pde.ino
+++ dht11_temp_sensor/dht_test_pde/dht_test_pde.ino
@@ -6,49 +6,39 @@
 // http://arduino.cc/playground/Main/DHTLib
 //
 
-#include <dht.h>
+#include <DHT_U.h>
 
-dht DHT;
 
 #define DHT11_PIN 4 // Digital PIN 4
+
+DHT dht(DHT11_PIN, DHT11);
 
 int sampleCount = 0;
 
 void printHeader()
 {
   Serial.println();
-  Serial.println("Status\tHumidity (%)\tTemperature (C)");
+  Serial.println("Humidity (%)\tTemperature (C)");
 }
 
 void setup()
 {
   Serial.begin(9600);
   Serial.println("DHT TEST PROGRAM ");
-  Serial.print("LIBRARY VERSION: ");
-  Serial.println(DHT_LIB_VERSION);
   Serial.println(__DATE__ " " __TIME__);
   printHeader();
+  dht.begin();
 }
 
 void loop()
 {
-  // READ DATA
-  int chk = DHT.read11(DHT11_PIN);
-  switch (chk)
-  {
-    case 0:  Serial.print("OK\t"); break;
-    case -1: Serial.print("Checksum error\t"); break;
-    case -2: Serial.print("Time out error\t"); break;
-    default: Serial.print("Unknown error\t"); break;
-  }
-
   // DISPLAY DATA
-  Serial.print(DHT.humidity,1);
+  Serial.print(dht.readHumidity(),1);
   Serial.print("\t\t");
-  Serial.println(DHT.temperature,1);
+  Serial.println(dht.readTemperature(),1);
 
   ++sampleCount;
-  if (sampleCount > 10) {
+  if (sampleCount > 20) {
       printHeader();
       sampleCount = 0;
   }
